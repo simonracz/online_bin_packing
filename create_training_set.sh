be15c2778e98209280512785db1a4da139ddabf5
Simon Racz <simonracz@gmail.com>
2016-11-19 08:49:03 +0100

    Added create_training_set.sh script.

--- create_training_set.sh
+++ create_training_set.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Usage:\n        $0 output_file"
 
 #base
-for i in `seq 0 0.1 0.8`;
+for i in `seq 0 0.1 0.9`;
 do
 	for j in `seq 1 20`
 	do
-		./generate -l 10 -r $i | ./annotate --auto
+		./generate -l 12 -r $i | ./annotate --auto -f $1
 	done
 done
 
 #core
-for i in `seq 0 0.1 0.3`;
+for i in `seq 0 0.1 0.4`;
 do
 	for j in `seq 1 50`
 	do
-		./generate -l 10 -r $i | ./annotate --auto
+		./generate -l 12 -r $i | ./annotate --auto -f $1
 	done
 done
